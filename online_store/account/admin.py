6d566bf8373ccf24b07c47c124ab8e3de5a2717e
Vlad <threeeyesvl@gmail.com>
2022-01-19 18:12:27 +0300

    Refactor: account models, admin

--- online_store/account/admin.py
+++ online_store/account/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Profile
+# from django.conf import settings
+# from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+# class UserInline(admin.TabularInline):
+#     model = get_user_model()
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'photo', 'town', 'street', 'house', 'building', 'apartment']
+    list_select_related = (
+        'user',
+    )
--- online_store/account/models.py
+++ online_store/account/models.py
@@ -3,9 +3,28 @@ from django.db import models
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    date_of_birth = models.DateField(blank=True, null=True)
-    photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, verbose_name="пользователь", on_delete=models.CASCADE)
+    photo = models.ImageField(verbose_name="Фото", upload_to="users/%Y/%m/%d/", blank=True)
+    town = models.CharField(
+        verbose_name="город", help_text="Not Required", max_length=255, blank=True, null=True
+    )
+    street = models.CharField(
+        verbose_name="улица", help_text="Not Required", max_length=255, blank=True, null=True
+    )
+    house = models.PositiveSmallIntegerField(
+        verbose_name="дом", help_text="Not Required", blank=True, null=True
+    )
+    building = models.PositiveSmallIntegerField(
+        verbose_name="корпус", help_text="Not Required", blank=True, null=True
+    )
+    apartment = models.PositiveSmallIntegerField(
+        verbose_name="квартира", help_text="Not Required", blank=True, null=True
+    )
 
     def __str__(self):
-        return f"Profile for user {self.user.username}"
+        return f"Profile for user {self.user}"
+
+    class Meta:
+        ordering = ("-user",)
+        verbose_name = "пользователь"
+        verbose_name_plural = "пользователи"
