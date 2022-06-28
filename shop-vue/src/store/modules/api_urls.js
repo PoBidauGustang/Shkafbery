14f288e4167a8d898c37b475394ef4a896b16eaa
Vlad <threeeyesvl@gmail.com>
2022-06-29 01:03:40 +0300

    fix(vue): add prod IP to url store

--- shop-vue/src/store/modules/api_urls.js
+++ shop-vue/src/store/modules/api_urls.js
@@ -1,9 +1,14 @@
 const state = {
-  backendShopUrl: "http://localhost:8000/api/shop",
-  backendBlogUrl: "http://localhost:8000/api/blog",
-  backendClosetUrl: "http://localhost:8000/api/closet",
-  backendInformationUrl: "http://localhost:8000/api/information",
-  backendAuthUrl: "http://localhost:8000/auth/token",
+  // backendShopUrl: "http://localhost:8000/api/shop",
+  // backendBlogUrl: "http://localhost:8000/api/blog",
+  // backendClosetUrl: "http://localhost:8000/api/closet",
+  // backendInformationUrl: "http://localhost:8000/api/information",
+  // backendAuthUrl: "http://localhost:8000/auth/token",
+  backendShopUrl: "http://185.46.11.192:8000/api/shop",
+  backendBlogUrl: "http://185.46.11.192:8000/api/blog",
+  backendClosetUrl: "http://185.46.11.192:8000/api/closet",
+  backendInformationUrl: "http://185.46.11.192:8000/api/information",
+  backendAuthUrl: "http://185.46.11.192:8000/auth/token",
 };
 const getters = {
   getServerShopUrl(state) {
